1ee2e0556f2ba0b64dda27956928edd6df7b6725
imrankhanf41 <imranbutex41@gmail.com>
2021-08-21 11:47:28 +0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,61 @@
+function getFirstInput(inputId,value,memorysize){
+    const inputValue = document.getElementById(inputId);
+    inputValue.value=value;
+    const inputFirstValue = inputValue.value;
+    const memory = document.getElementById(memorysize);
+    let memoryPrice =  memory.innerText;
+    memory.innerText =inputFirstValue;
+    calculateTotal()
+}
+
+document.getElementById("eight-gb-memory" ).addEventListener("click",function(){
+     getFirstInput("eight-gb-memory",0,"memory")  
+});
+document.getElementById("sixteent-gb-memory").addEventListener("click",function(){
+getFirstInput("sixteent-gb-memory",180,"memory");
+})
+
+document.getElementById("two-fifty-six").addEventListener("click",function(){
+    getFirstInput("two-fifty-six",0,"memory-ssd");
+})
+document.getElementById("five-one-two" ).addEventListener("click",function(){
+    getFirstInput("five-one-two" ,100,"memory-ssd"); 
+})
+document.getElementById("one-tera-bite" ).addEventListener("click",function(){
+    getFirstInput("one-tera-bite" ,180,"memory-ssd");
+})
+document.getElementById("Aug-25-delivery-charge").addEventListener("click",function(){
+    getFirstInput("Aug-25-delivery-charge" ,0,"delivery-charge");
+})
+document.getElementById("Aug-21-delivery-charge").addEventListener("click",function(){
+    getFirstInput("Aug-21-delivery-charge" ,20,"delivery-charge");
+})
+ function getValue(product){
+     const price = document.getElementById(product);
+    const priceAmount = price.innerText;
+  const PriceValue = parseInt(priceAmount);
+    return PriceValue;  
+ }
+ function calculateTotal(){
+ const bestPrice = getValue("best-price");
+ const extraMemoryPrice = getValue("memory");
+const  ssdPriceValue = getValue("memory-ssd");
+const deliveryValue = getValue("delivery-charge");
+ console.log(bestPrice)
+ console.log(extraMemoryPrice)
+console.log(ssdPriceValue)
+console.log(deliveryValue)
+const totalPrice = bestPrice  + extraMemoryPrice + ssdPriceValue + deliveryValue;
+document.getElementById("total-price").innerText = totalPrice;
+ }
+
+ document.getElementById("button-apply").addEventListener("click",function(){
+     const inputValue = document.getElementById("cupon code");
+     const inputCode = inputValue.value;
+     if(inputCode == "stevekaku"){
+         const totalPrice = document.getElementById("total-price").innerText;
+         const discount = totalPrice *(20/100);
+         const total = totalPrice - discount;
+         document.getElementById("final-total").innerText = total;
+     }
+ })
